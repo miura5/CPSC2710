0402b9178c4896f8af68c58b262d992a2dd72c0c
Mathew Nagle <mathewnagle@Mathews-Air.lan>
2024-08-27 15:35:33 -0500

    Add methods and main class

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -3,4 +3,46 @@ public class SeatReservation {
     private java.time.LocalDateTime flightTime;
     private String firstName;
     private String lastName;
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    public void setFlightDesignator(String fd) {
+        this.flightDesignator = fd;
+    }
+
+    public java.time.LocalDateTime getFlightTime() {
+        return flightTime;
+    }
+
+    public void setFlightTime(java.time.LocalDateTime date) {
+        this.flightTime = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String ln) {
+        this.lastName = ln;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SeatReservation{flightDesignator=%s,flightDate=%s, firstName=%s, lastName=%s}",
+                flightDesignator,
+                flightTime,
+                firstName,
+                lastName
+        );
+    }
 }
---
+++ module1/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        SeatReservation seatReservation = new SeatReservation();
+        System.out.println(seatReservation.toString());
+    }
+}
