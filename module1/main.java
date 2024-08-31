4d1754953f9ef4ad9bc1617f8a57286ab4fffb33
Mathew Nagle <mathewnagle@Mathews-Air.lan>
2024-08-31 11:16:40 -0500

    Add module 2 IntelliJ template

--- module1/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        SeatReservation seatReservation = new SeatReservation();
-        seatReservation.setFlightDesignator("1234567");
-        System.out.println(seatReservation.toString());
-    }
-}
